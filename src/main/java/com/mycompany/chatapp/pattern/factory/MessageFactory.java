9c648e61ac3d5fbff1d2e629535d1f4eb63668ba
Nour Yasser <nn428878@gmail.com>
2025-12-18 03:41:27 +0200

    Add Messages Factory

--- src/main/java/com/mycompany/chatapp/pattern/factory/MessageFactory.java
+++ src/main/java/com/mycompany/chatapp/pattern/factory/MessageFactory.java
@@ -1,9 +1,35 @@
 package com.mycompany.chatapp.pattern.factory;
 
+import com.mycompany.chatapp.model.Message;
+
 /**
  * Factory Pattern - MessageFactory
 
  */
 public class MessageFactory {
-    // Implement Factory pattern here
+
+    public static Message createMessage( String type, int senderId,
+            int receiverId, String data ) {
+
+        if (type == null) {
+            throw new IllegalArgumentException("Message type cannot be null");
+        }
+
+        switch (type.toUpperCase()) {
+            case "TEXT":
+                return new TextMessage(senderId, receiverId, data);
+
+            case "IMAGE":
+                return new ImageMessage(senderId, receiverId, data);
+
+            case "VIDEO":
+                return new VideoMessage(senderId, receiverId, data);
+
+            default:
+                throw new IllegalArgumentException("Unsupported message type: " + type);
+        }
+    }
 }
+
+
+
