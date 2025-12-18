e6342cdde17d31f1ba4a275bf5229d934eb61734
Hayam Mostafa <hayammostafa2877@gmail.com>
2025-12-18 08:37:42 +0200

    Command Pattern

--- src/main/java/com/mycompany/chatapp/pattern/command/DeleteMessageCommand.java
+++ src/main/java/com/mycompany/chatapp/pattern/command/DeleteMessageCommand.java
@@ -3,7 +3,7 @@ package com.mycompany.chatapp.pattern.command;
 import com.mycompany.chatapp.model.Message;
 
 public class DeleteMessageCommand implements ICommand{
-    //  Implement DeleteMessageCommand here
+
     private Message message;
     
     public DeleteMessageCommand(Message message){
